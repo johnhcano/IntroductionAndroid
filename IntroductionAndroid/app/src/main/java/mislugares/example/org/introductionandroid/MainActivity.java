a2d5a49577f493da8f19a7267cfc539e978bf7d8
JhonHCanoB <johnhcano@hotmail.com>
2015-08-29 22:08:23 -0500

    Initial commit

---
+++ IntroductionAndroid/app/src/main/java/mislugares/example/org/introductionandroid/MainActivity.java
@@ -0,0 +1,54 @@
+package mislugares.example.org.introductionandroid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ActionBarActivity {
+
+    private EditText editText;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void cambiarTexto (View v){
+        String mostrar = editText.getText().toString();
+        textView.setText(mostrar);
+        //mostrar texto mediante un Toast
+        Toast.makeText(MainActivity.this, "Cadena = " + mostrar, Toast.LENGTH_LONG).show();
+    }
+}
